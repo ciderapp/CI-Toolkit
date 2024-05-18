473c0b6d152c9099c51e816f305cb34cc95235ea
cryptofyre <cryptofyre@cryptofyre.org>
2024-05-18 18:06:45 -0500

    kek

--- fixffmpeg.sh
+++ fixffmpeg.sh
@@ -12,6 +12,7 @@ apt remove -y ffmpeg
 # Exchange the default ffmpeg deb with the one from HTTP and install it, link it to /usr/bin/ffmpeg
 wget https://cdn.cider.sh/jellyfin-ffmpeg6_6.0.1-6-bullseye_amd64.deb
 dpkg -i jellyfin-ffmpeg6_6.0.1-6-bullseye_amd64.deb
+rm jellyfin-ffmpeg6_6.0.1-6-bullseye_amd64.deb
 ln -s /usr/lib/jellyfin-ffmpeg/ffmpeg /usr/bin/ffmpeg
 
 # Install i965 driver (default gpu driver, uncomment if you have <9th gen CPU)
