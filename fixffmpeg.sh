b4b3a6ce2df899e29ffd6ae3ed56bc1c6a7cc153
cryptofyre <cryptofyre@cryptofyre.org>
2024-05-18 18:09:32 -0500

    loosely pick out bits.

--- fixffmpeg.sh
+++ fixffmpeg.sh
@@ -13,14 +13,15 @@ apt remove -y ffmpeg
 wget https://cdn.cider.sh/jellyfin-ffmpeg6_6.0.1-6-bullseye_amd64.deb
 dpkg -i jellyfin-ffmpeg6_6.0.1-6-bullseye_amd64.deb
 rm jellyfin-ffmpeg6_6.0.1-6-bullseye_amd64.deb
-ln -s /usr/lib/jellyfin-ffmpeg/ffmpeg /usr/bin/ffmpeg
 
 # Install i965 driver (default gpu driver, uncomment if you have <9th gen CPU)
 # apt install i965-va-driver-shaders
 
-wget -qO - https://repositories.intel.com/graphics/intel-graphics.key | apt-key add -
-echo 'deb [arch=amd64] https://repositories.intel.com/graphics/ubuntu focal main' >> /etc/apt/sources.list
 apt update
 apt install -y intel-media-va-driver-non-free
 
+# Link and cleanup.
+
+apt --fix-missing install -y
 apt --fix-broken install -y
+ln -s /usr/lib/jellyfin-ffmpeg/ffmpeg /usr/bin/ffmpeg
