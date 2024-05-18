c73dfdc2a320c3d1e880a85cab984df90b604d4b
cryptofyre <cryptofyre@cryptofyre.org>
2024-05-18 17:14:51 -0500

    Update to pull from cdn.

--- fixffmpeg.sh
+++ fixffmpeg.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
+# This script is intended to fix the ffmpeg issue on Cider-Bot-Private
+# It will install the correct ffmpeg version and the necessary dependencies
+# It will also install the correct GPU drivers for Intel QuickSync
+# Copyright (C) 2024 Cider Collective
 
-# Install debug tools
+# Install debug tools / dependencies
 apt update
-apt install -y wget gnupg vainfo gpg-agent
+apt install -y wget gnupg vainfo gpg-agent libllvm11
 
-# Upgrade jellyfin-ffmpeg to latest version 4.4.1-4-bullseye (increases transcoding perfs a lot)
-echo "deb http://http.us.debian.org/debian stable main contrib non-free" | tee -a /etc/apt/sources.list
-apt update
-apt install -y -u jellyfin-ffmpeg
+# Exchange the default ffmpeg deb with the one from HTTP and install it, link it to /usr/bin/ffmpeg
+wget https://cdn.cider.sh/jellyfin-ffmpeg6_6.0.1-6-bullseye_amd64.deb
+dpkg -i jellyfin-ffmpeg6_6.0.1-6-bullseye_amd64.deb
+ln -s /usr/lib/jellyfin-ffmpeg/ffmpeg /usr/bin/ffmpeg
 
 # Install i965 driver (default gpu driver, uncomment if you have <9th gen CPU)
 # apt install i965-va-driver-shaders
@@ -17,3 +21,4 @@ wget -qO - https://repositories.intel.com/graphics/intel-graphics.key | apt-key 
 echo 'deb [arch=amd64] https://repositories.intel.com/graphics/ubuntu focal main' >> /etc/apt/sources.list
 apt update
 apt install -y intel-media-va-driver-non-free
+
